53230e1587e6dac3d4c785f875c3d8f8e2459ce4
LeeJongbokZ <s2feeling@naver.com>
2024-11-07 14:32:11 +0900

    first commit

---
+++ src/main/java/com/example/concertTicket_prometheus/ConcertTicketPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.example.concertTicket_prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTicketPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertTicketPrometheusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/concertTicket_prometheus/ConcertTicketPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concertTicket_prometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTicketPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
